5226bd11801afebf43ef2e3d218f57db9e684439
Shoyaib Rahaman <iamshoyaib07@gmail.com>
2025-09-22 10:55:01 +0530

    Update algorithms.py
    New Algorithm added

--- algorithms.py
+++ algorithms.py
@@ -7,3 +7,17 @@ def bubble_sort(data_list):
             if data_list[j] > data_list[j+1]:
                 data_list[j], data_list[j+1] = data_list[j+1], data_list[j]
             yield data_list,(j,j+1)
+
+#Insertion Sort
+
+def insertion_sort(data_list):
+    for i in range(1, len(data_list)):
+        key = data_list[i]
+        j = i - 1
+        while j >= 0 and key < data_list[j]:
+            data_list[j + 1] = data_list[j]
+            yield data_list, (i, j, -1)
+            j -= 1
+        data_list[j + 1] = key
+        yield data_list, (i, j + 1, -2)
+    yield data_list, ()            
