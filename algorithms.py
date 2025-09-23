2cd957b3580a66dc9edeac550db16047a40a3afc
Shoyaib Rahaman <iamshoyaib07@gmail.com>
2025-09-23 13:39:15 +0530

    Update algorithms.py
    Added Selection Sort Algorithm.

--- algorithms.py
+++ algorithms.py
@@ -21,3 +21,17 @@ def insertion_sort(data_list):
         data_list[j + 1] = key
         yield data_list, (i, j + 1, -2)
     yield data_list, ()            
+
+# Selection Sort
+
+def selection_sort(data_list):
+    n = len(data_list)
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            if data_list[j] < data_list[min_idx]:
+                min_idx = j
+            yield data_list, (j, min_idx)
+        data_list[i], data_list[min_idx] = data_list[min_idx], data_list[i]
+        yield data_list, (i, min_idx)
+    yield data_list, ()  
